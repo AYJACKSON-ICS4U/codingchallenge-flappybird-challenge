7e6010c26b533efcb597a914f63af9f94a2b7fbe
Sydney Caulfeild <31681144+Scaul1@users.noreply.github.com>
2018-01-09 11:50:55 -0500

    Update sketch-code.js

--- Individual Challenges/Syd/sketch-code.js
+++ Individual Challenges/Syd/sketch-code.js
@@ -1,9 +1,20 @@
-//here is my sketch code. I have a black canvas
+var bird;
 function setup() {
     createCanvas(400, 600);
     console.log("Check, Check");
+    bird = new Bird();
 }
 
 function draw() {
     background(0);
+    bird.update();
+    bird.show();
+}
+
+function keyPressed() {
+    if (key ==' '){
+        bird.up();
+        //testing that space bar works
+        //console.log("SPACE");
+    }
 }
