15503e23add88f278af288c45703c4e2c0fe7b15
Steph <31697395+Sbakr@users.noreply.github.com>
2018-01-09 11:51:06 -0500

    Update sketch.js
    4:50 time

--- Individual Challenges/Steph/sketch.js
+++ Individual Challenges/Steph/sketch.js
@@ -1,7 +1,22 @@
+var bird;
 function setup() {
-  createCanvas(400, 600);
+    createCanvas(400, 600);
+    background(mouseX, 100, 100);
+    bird = new Bird();
 }
 
 function draw() {
-  background(mouseX, 100, 100);
+    background(0);
+    bird.update();
+    bird.show();
 }
+
+function keyPressed() {
+    if(key == ' '){
+        console.log("SpaceBar");
+    }
+}
+
+
+
+4:40 time
