2cdf569c8f4c71f523ec8e0417854ae7e6454550
Steph <31697395+Sbakr@users.noreply.github.com>
2018-01-10 10:30:57 -0500

    Update sketch.js

--- Individual Challenges/Steph/sketch.js
+++ Individual Challenges/Steph/sketch.js
@@ -1,22 +1,43 @@
 var bird;
+var pipes = [];
+
 function setup() {
     createCanvas(400, 600);
-    background(mouseX, 100, 100);
     bird = new Bird();
+    pipes.push(new Pipe());
 }
 
 function draw() {
     background(0);
+    
+    for (var i = pipes.length-1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+        
+        if (pipes[i].hits(bird)) {
+            console.log("HIT");
+        }
+        
+        if (pipes[i].offscreen()){
+            pipes.splice(i, 1);
+        }
+    }
+    
     bird.update();
     bird.show();
+    
+    if (frameCount % 100 == 0) {
+        pipes.push(new Pipe()); 
+    }
 }
 
 function keyPressed() {
     if(key == ' '){
-        console.log("SpaceBar");
+        bird.up();
+        //console.log("SpaceBar");
     }
 }
 
 
 
-4:40 time
+17:17 time
