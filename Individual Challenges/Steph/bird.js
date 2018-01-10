c9a5615b2da040c5f9b39eb3263d56a273af1c9e
Steph <31697395+Sbakr@users.noreply.github.com>
2018-01-10 10:30:31 -0500

    Update bird.js

--- Individual Challenges/Steph/bird.js
+++ Individual Challenges/Steph/bird.js
@@ -2,7 +2,8 @@ function Bird() {
     this.y = height/2;
     this.x = 65;
     
-    this.gravity = 1;
+    this.gravity = 0.1;
+    this.lift = -15;
     this.velocity = 0;
     
     this.show = function(){
@@ -10,8 +11,13 @@ function Bird() {
         ellipse(this.x,this.y, 30, 30);
     }
     
+    this.up = function() {
+        this.velocity += this.lift;
+    }
+    
     this.update = function() {
         this.velocity += this.gravity;
+        this.velocity += 0.9;
         this.y += this.velocity;
         
         if (this.y > height) {
@@ -25,3 +31,4 @@ function Bird() {
         }
     }
 }
+
