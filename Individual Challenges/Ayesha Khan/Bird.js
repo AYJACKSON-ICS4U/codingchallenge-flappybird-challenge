02f8326d0d9e31bd8a5127f135b432117205562a
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-10 10:15:37 -0500

    Create Bird.js

---
+++ Individual Challenges/Ayesha Khan/Bird.js
@@ -0,0 +1,37 @@
+//bird function
+
+function Bird() {
+  this.y = height/2;
+  this.x = 64;
+
+  this.gravity = 0.6;
+  this.lift = -13;
+  this.velocity = 0;
+
+  this.show = function(){
+    fill(255);
+    ellipse( this.x, this.y, 32, 32);
+  }
+
+  this.up = function(){
+    this.velocity += this.lift;
+  }
+
+  this.update = function(){
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity*= 0.9;
+
+    if (this. y > height){
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this. y < 0){
+      this.y = 0;
+      this.velocity = 0;
+    }
+  }
+
+
+}
