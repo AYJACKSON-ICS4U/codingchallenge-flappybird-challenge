cbb70538b5ad06264b8089f2c3195a251d55c0b1
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-14 18:50:34 -0500

    Update Pipe.js

--- Individual Challenges/Ayesha Khan/My Code/Pipe.js
+++ Individual Challenges/Ayesha Khan/My Code/Pipe.js
@@ -7,15 +7,38 @@ function Pipe() {
   this.w = 20;
   this.speed = 2;
 
+  this.highlight = false;
+
+
+  this.hits = function(bird){
+    if (bird.y < this.top || this.y > height - this.bottom){
+        if (bird.x > this.x && bird.x < this.x + this.w){
+          this.highlight = true;
+          return true;
+        }
+    }
+    this.highlight = false;
+    return false;
+  }
 
   this.show = function() {
     fill(255);
-    rect(this.x, 0, this.w, this.top);
-    rect(this.x, height-this.bottom, this.w, this.bottom);
+    if(this.highlight){
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top); //top rectangle
+    rect(this.x, height-this.bottom, this.w, this.bottom); //Bottom rectangle
   }
 
   this.update = function() {
     this.x -= this.speed;
+  }
 
+  this.offscreen = function(){
+    if(this.x < -this.w){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
