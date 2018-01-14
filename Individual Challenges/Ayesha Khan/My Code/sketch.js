f3ceb4f9e906d5c5f467f352b5f0d47446ffdb45
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-14 18:50:15 -0500

    Update sketch.js

--- Individual Challenges/Ayesha Khan/My Code/sketch.js
+++ Individual Challenges/Ayesha Khan/My Code/sketch.js
@@ -14,15 +14,24 @@ function setup () {
 function draw(){
   background(0);
 
+  for (var i = pipes.length-1; i >=0; i--){
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)){
+      console.log("HIT");
+    }
+
+
+    if (pipes[i].offscreen()){
+      pipes.splice(i, 1);
+    }
+  }
+
   bird.update();
   bird.show();
 
-  for (var i = 0; i < pipes.length; i++){
-    pipes[i].show();
-    pipes[i].update();
-  }
-
-  if (frameCount % 40 == 0){
+  if (frameCount % 100 == 0){
     pipes.push(new Pipe());
   }
 
