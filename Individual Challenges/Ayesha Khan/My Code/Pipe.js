b1dc6a12852daca517e248bc7c7adbb8ffae745b
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-15 10:57:35 -0500

    Update Pipe.js

--- Individual Challenges/Ayesha Khan/My Code/Pipe.js
+++ Individual Challenges/Ayesha Khan/My Code/Pipe.js
@@ -10,7 +10,7 @@ function Pipe() {
 
 
   this.hits = function(bird){
-    if (bird.y < this.top || this.y > height - this.bottom){
+    if (bird.y < this.top || bird.y > height - this.bottom){
         if (bird.x > this.x && bird.x < this.x + this.w){
           this.highlight = true;
           return true;
