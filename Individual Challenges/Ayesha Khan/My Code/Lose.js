a73868c5cd8973e6d2b1c4a118e837800b4b7120
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-16 10:23:03 -0500

    Update Lose.js

--- Individual Challenges/Ayesha Khan/My Code/Lose.js
+++ Individual Challenges/Ayesha Khan/My Code/Lose.js
@@ -1,15 +1,15 @@
 //lose constructor
-var count;
+
 function Lose () {
   createCanvas(400, 600);
-  //sc = new Score();
 
-  this.page = function(count)
+
+  this.page = function()
   {
     background(0);
      text("YOU LOSE", 150, 200);
      text("SCORE:", 150, 230);
-     text(count, 150, 260);
+     Score.c();
      textSize(30);
      fill(255);
   }
