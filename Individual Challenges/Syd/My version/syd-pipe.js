631e178ec8b99ceb7816b4abfc51c2e5192f4248
Sydney Caulfeild <31681144+Scaul1@users.noreply.github.com>
2018-01-16 10:20:30 -0500

    Update syd-pipe.js

--- Individual Challenges/Syd/My version/syd-pipe.js
+++ Individual Challenges/Syd/My version/syd-pipe.js
@@ -1,10 +1,12 @@
+
+
 function Pipe() {
     this.top = random(height/2);
     this.bottom = random(height/2);
     this.x = width;
     this.w = 20;
-    this.speed = 2;
-    this.score = 0;
+    this.speed = 0.5;
+    this.s = 0;
     
     this.highlight = false;
     
@@ -15,22 +17,28 @@ function Pipe() {
                 return true;
             }
         }
-        this.score +=1; 
+ 
         this.highlight = false;
         return false;
     }
+    
     
     this.show = function() {
         fill(235, 172, 242);
         if (this.highlight === true){
             fill(255, 0, 0);
         }
+        
         rect(this.x, 0, this.w, this.top);
         rect(this.x, height-this.bottom, this.w, this.bottom);
     }
     
     this.update = function() {
         this.x -= this.speed;
+        
+        if (count % 10 == 0){
+            this.speed += 0.1;
+        }
     }
     
     this.offscreen = function() {
