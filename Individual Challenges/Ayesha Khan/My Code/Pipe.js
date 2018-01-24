efaaaa0518acf19c1fcf458e97754ed135f6fb90
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-23 22:01:19 -0500

    Update Pipe.js

--- Individual Challenges/Ayesha Khan/My Code/Pipe.js
+++ Individual Challenges/Ayesha Khan/My Code/Pipe.js
@@ -1,53 +1,3 @@
-//Pipe constructor object
-
-function Pipe() {
-  this.top = random (height/2);
-  this.bottom = random (height/2);
-  this.x = width;
-  this.w = 20;
-  this.speed = 2;
-  this.highlight = false;
-
-
-  this.hits = function(bird){
-    if (bird.y < this.top || bird.y > height - this.bottom){
-        if (bird.x > this.x && bird.x < this.x + this.w){
-          this.highlight = true;
-          return true;
-        }
-    }
-    this.highlight = false;
-    return false;
-  }
-
-  this.crosses = function(bird){
-    if (bird.x > this.x && bird.x < this.x + this.w){
-      return true;
-    }
-  }
-
-  this.show = function() {
-    fill(255);
-    if(this.highlight){
-      fill(255, 0, 0);
-    }
-    rect(this.x, 0, this.w, this.top); //top rectangle
-    rect(this.x, height-this.bottom, this.w, this.bottom); //Bottom rectangle
-  }
-
-  this.update = function() {
-    this.x -= this.speed;
-  }
-
-  this.offscreen = function(){
-    if(this.x < -this.w){
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
 /////////////////////////////////////////
 //Pipe constructor object
 
@@ -72,8 +22,8 @@ function Pipe() {
   }
 
   this.crosses = function(bird){
-  if (bird.y < this.top && bird.y > height - this.bottom){
-    if (bird.x === this.x || bird.x === this.x + this.w+1){
+    if (bird.y > this.top && bird.y < height - this.bottom){ 
+        if (bird.x === this.x || bird.x === this.x + this.w+1){
       return true;
     }
    }
@@ -100,4 +50,3 @@ function Pipe() {
     }
   }
 }
-
