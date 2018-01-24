8ce3fe9ba1b631099c94a5cc6a23719a4e5560ae
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-23 21:56:04 -0500

    Update sketch.js

--- Individual Challenges/Ayesha Khan/My Code/sketch.js
+++ Individual Challenges/Ayesha Khan/My Code/sketch.js
@@ -1,5 +1,5 @@
 //Ayesha Khan
-//1/9/18
+//1/23/18
 
 var bird;
 var pipes = [];
@@ -24,7 +24,7 @@ function draw(){
 
     if (pipes[i].hits(bird)){
       console.log("got it");
-      lose.page(score.cu());
+      score.loss();
     }
     else if (pipes[i].crosses(bird))
     {
@@ -51,62 +51,3 @@ function keyPressed(){
     //console.log("SPACE");
   }
 }
-//////////////////////////////////////////////////////
-
-
-//Ayesha Khan
-//1/9/18
-
-var bird;
-var pipes = [];
-var score;
-var lose;
-var count = 0;
-function setup () {
-  createCanvas(400, 600);
-  bird = new Bird();
-  score = new Score();
-  lose = new Lose();
-  pipes.push(new Pipe());
-}
-
-
-
-function draw(){
-  background(0);
-  score.c();
-  count = score.cu.s;
-  for (var i = pipes.length-1; i >=0; i--){
-    pipes[i].show();
-    pipes[i].update();
-
-    if (pipes[i].hits(bird)){
-      console.log("got it");
-      score.loss();
-    }
-   else if (pipes[i].crosses(bird))
-    {
-          score.cu();
-    }
-
-    if (pipes[i].offscreen()){
-      pipes.splice(i, 1);
-    }
-  }
-
-  bird.update();
-  bird.show();
-
-  if (frameCount % 100 == 0){
-    pipes.push(new Pipe());
-  }
-
-}
-
-function keyPressed(){
-  if (key ==' '){
-    bird.up();
-    //console.log("SPACE");
-  }
-}
-
